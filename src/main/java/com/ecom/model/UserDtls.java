31ba2d4a37081c0bd478f9fbff788dcc83832d92
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2025-08-05 23:09:38 +0530

    updated the login and forget password functionality add email reset mail

--- src/main/java/com/ecom/model/UserDtls.java
+++ src/main/java/com/ecom/model/UserDtls.java
@@ -1,5 +1,7 @@
 package com.ecom.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -37,5 +39,17 @@ public class UserDtls {
     private String password;
 
     private String profileImage;
+
+    private String role;
+
+    private Boolean isEnable;
+
+    private Boolean accountNonLocked;
+
+    private Integer failedAttempt;
+
+    private Date lockTime;
+
+    private String resetToken;
 
 }
